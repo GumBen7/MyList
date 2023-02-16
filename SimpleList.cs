2bd4bbdf97909e6261d53959f3f9082e5a10c718
Егор Сивцев <gumben7@gmail.com>
2023-02-16 11:37:16 +0700

    prefinal

--- SimpleList.cs
+++ SimpleList.cs
@@ -1,7 +1,7 @@
 public class SimpleList<T> {
     public SimpleList(T data) {
         this._head = new Node<T>(data);
-        this._last = this._head;
+        this._last = new Node<T>();
     }
     private Node<T> _head;
     private Node<T> _last;
@@ -11,7 +11,18 @@ public class SimpleList<T> {
         this._last = this._last.Next;
     }
 
-    /*public T Pop() {
-        return new T;
-    }//*/
+    public T Pop() {
+        if (!IsEmpty) {
+            Node<T> popped = this._head;
+            T data = popped.Data;
+            this._head = this._head.Next;
+            popped = null;
+            return data;
+        }
+        return null;
+    }
+    
+    public bool IsEmpty() {
+        return _head == _last;
+    }
 }
