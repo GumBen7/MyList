7320d57610f6b84c1429de3af4027c51e7e12370
George Sivtsev <gumben7@gmail.com>
2023-02-20 14:35:31 +0700

    proper push

--- SimpleList.cs
+++ SimpleList.cs
@@ -1,23 +1,19 @@
 public class SimpleList<T> {
     public SimpleList() {
-        this._head = new Node<T>();
+        this._head = new Node<T>(default(T), new Node<T>());
         this._penult = this._head;
     }
     private Node<T> _head;
     private Node<T> _penult;
 
     public void Push(T data) {
-        this._penult.Next = new Node<T>(data);
+        this._penult.SetNext(new Node<T>(data, this._penult.Next));
         this._penult = this._penult.Next;
     }
 
     public T Pop() {
         if (!IsEmpty()) {
-            Node<T> popped = this._head;
-            T data = popped.Data;
-            this._head = this._head.Next;
-            popped = null;
-            return data;
+            
         }
         return default(T);
     }
